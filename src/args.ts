939eb8f8706df616076701c5543a3e6f6957a58a
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-13 10:09:37 +0200

    fix help on no args

--- src/args.ts
+++ src/args.ts
@@ -16,17 +16,21 @@ export function parseArgs(): {
     .description("Run the Wasp application")
     .version(packageJson.version)
     .addArgument(
-      new Argument("<mode>", "The run mode").choices(["dev", "build"])
+      new Argument("[mode]", "The run mode").choices(["dev", "build"])
     )
     .option("--path-to-app <path>", "Path to the Wasp application", ".")
     .option("--wasp-cli-cmd <command>", "Wasp CLI command to use", "wasp")
     .parse();
 
   const options = parsedProgram.opts();
-  const args = parsedProgram.processedArgs;
+  const [mode] = parsedProgram.processedArgs;
+
+  if (mode === undefined) {
+    parsedProgram.help();
+  }
 
   return {
-    mode: args[0],
+    mode: mode as Mode,
     pathToApp: options.pathToApp as PathToApp,
     waspCliCmd: options.waspCliCmd as WaspCliCmd,
   };
